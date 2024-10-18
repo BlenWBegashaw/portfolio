d589be4c64a42acbec670d1355b30a15c06cb209
Blen Begashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-10-17 20:01:48 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Example of adding interactivity
+    const button = document.querySelector('.animated-button');
+    button.addEventListener('click', () => {
+        alert('You clicked the button!');
+    });
+});
